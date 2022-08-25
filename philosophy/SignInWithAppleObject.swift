edde5844addbc97ca839908ea198b7db8a90f597
市川マサル <>
2022-08-26 00:36:04 +0900

    firstcommit

---
+++ philosophy/SignInWithAppleObject.swift
@@ -0,0 +1,20 @@
+//
+//  SignInWithAppleObject.swift
+//  philosophy
+//
+//  Created by 市川マサル on 2022/08/24.
+//
+
+import SwiftUI
+
+struct SignInWithAppleObject: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignInWithAppleObject_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInWithAppleObject()
+    }
+}
---
+++ philosophy/testdb.swift
@@ -0,0 +1,20 @@
+//
+//  testdb.swift
+//  philosophy
+//
+//  Created by 市川マサル on 2022/08/26.
+//
+
+import SwiftUI
+
+struct testdb: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct testdb_Previews: PreviewProvider {
+    static var previews: some View {
+        testdb()
+    }
+}
